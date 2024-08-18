1b32c3d538fb5297ee623a6bdc1cd032c7eb97c0
y.kurita <kurita.yuya1@gmail.com>
2024-08-18 17:43:09 +0900

    first commit

---
+++ ABC/abc074/a.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<list>
+#include<map>
+#include <cstdlib>
+#include<math.h>
+#include <cmath>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+
+int main(){
+    ll n,a;
+    cin >> n >> a;
+    cout << n*n - a << endl;
+    return 0;
+}
---
+++ ABC/abc084/a.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<list>
+#include<map>
+#include <cstdlib>
+#include<math.h>
+#include <cmath>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+
+int main(){
+    ll n;
+    cin >> n;
+    cout << 48 - n << endl;
+    return 0;
+}
